7b13fc605e3dddfd38a78322c45872041f16544c
warbasher <pirey5@gmail.com>
2016-09-26 11:45:43 -0400

    Initial content
    Added initial content to the repo

---
+++ donation/README.md
@@ -0,0 +1,20 @@
+# README
+
+## Configuration
+
+Open `scripts/app.js` and edit the following section:
+
+````json
+cfg = {
+  payPal: {
+    endpoint: 'https://www.paypal.com/cgi-bin/webscr', // make sure to remove the sandbox part
+    business: 'yourpaypalemail'
+  }
+}
+````
+
+## Installation
+
+Upload the files to your static host.
+
+
---
+++ donation/scripts/app.js
@@ -0,0 +1,42 @@
+
+/**
+ * Config
+ */
+cfg = {
+  payPal: {
+    endpoint: 'https://www.paypal.com/cgi-bin/webscr',
+    business: 'pirey10@gmail.com'
+  }
+}
+
+
+/**
+ * On page load
+ */
+$( document ).ready( function() {
+  $( '#business' ).val( cfg.payPal.business );
+  $( '#donate' ).attr( 'action', cfg.payPal.endpoint );
+})
+
+
+var steamIdPattern = /^STEAM_[0-5]:[01]:\d{1,15}$/;
+
+/**
+ * Form validation
+ */
+$( "#donate" ).submit( function( e ) {
+  var $steamId = $( "#steamid" );
+  var $grp = $( "#steamid-grp" );
+
+  if ( steamIdPattern.test( $steamId.val() ) === false ) {
+    e.preventDefault();
+    $grp.addClass( 'has-danger' );
+    $steamId.addClass( 'form-control-danger')
+  } else {
+    $grp.removeClass( 'has-danger' );
+    $steamId.removeClass( 'form-control-danger' )  
+  }
+})
+
+
+
